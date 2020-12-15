2b7c9721f796820f5ff01babfdd4e61ee122679b
cdx1006 <2516783556@qq.com>
2020-12-15 17:51:37 +0800

    123

---
+++ 我的第二周课程/第二十二次课/.js
@@ -0,0 +1,8 @@
+var pitaya3 = {}
+console.log(typeof pitaya3)
+//引用类型 class
+//普通类型 int  char    double  float
+var pitaya4 = new Object();
+
+console.log(pitaya3 instanceof Object);
+console.log(pitaya4 instanceof Object);
